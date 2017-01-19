d49fcc1031a3102f5b1097f46a735bac60fcf3e2
Nicolás López <nicolaslopezj@me.com>
2017-01-19 16:31:33 -0300

    improve require role

--- packages/react-scripts/template/src/App/helpers/decorators/requireRole.js
+++ packages/react-scripts/template/src/App/helpers/decorators/requireRole.js
@@ -1,6 +1,22 @@
 import React from 'react'
 import includes from 'lodash/includes'
 
+const styles = {
+  container: {
+    textAlign: 'center',
+    padding: 20
+  },
+  notAllowed: {
+    fontSize: 20,
+    color: 'red'
+  },
+  needRole: {
+    fontSize: 14,
+    marginTop: 10,
+    color: '#5c5c5c'
+  }
+}
+
 export default function (role) {
   return function (ComposedComponent) {
     return class WithRoles extends React.Component {
@@ -11,8 +27,13 @@ export default function (role) {
 
       renderNotAllowed () {
         return (
-          <div>
-            Not allowed
+          <div style={styles.container}>
+            <div style={styles.notAllowed}>
+              Not allowed
+            </div>
+            <div style={styles.needRole}>
+              You need the role "role"
+            </div>
           </div>
         )
       }
