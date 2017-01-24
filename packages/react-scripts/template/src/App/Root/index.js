5d71295946bddda2ef6989acc7a05cb25175551f
Nicolás López <nicolaslopezj@me.com>
2017-01-24 11:25:33 -0300

    add orionsoft provider

--- packages/react-scripts/template/src/App/Root/index.js
+++ packages/react-scripts/template/src/App/Root/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import apolloClient from './apollo'
 import {ApolloProvider} from 'react-apollo'
 import MeProvider from './MeProvider'
+import OrionsoftProvider from 'orionsoft-parts/lib/components/Provider'
 import './locale'
 
 export default class Root extends React.Component {
@@ -13,9 +14,11 @@ export default class Root extends React.Component {
   render () {
     return (
       <ApolloProvider client={apolloClient}>
-        <MeProvider>
-          {this.props.children}
-        </MeProvider>
+        <OrionsoftProvider>
+          <MeProvider>
+            {this.props.children}
+          </MeProvider>
+        </OrionsoftProvider>
       </ApolloProvider>
     )
   }
