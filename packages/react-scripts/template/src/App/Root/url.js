50341f1c3d7b9787ad9fa26aa6baa2d9131560ca
Nicolás López <nicolaslopezj@me.com>
2017-03-17 14:02:54 -0300

    improve more things

--- packages/react-scripts/template/src/App/Root/url.js
+++ packages/react-scripts/template/src/App/Root/url.js
@@ -1,5 +1,5 @@
 
 const isProduction = window.location.hostname.includes('orionsoft.io')
-const baseURL = isProduction ? 'http://api.orionsoft.io' : `http://${window.location.hostname}:3010`
+const baseURL = isProduction ? 'http://api.orionsoft.io' : `http://${window.location.hostname}:3000`
 
 export default baseURL
